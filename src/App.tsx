fb82ef6100abb94748d6a91ecd9908ea7225abdb
erain <enesyagmur99@gmail.com>
2024-10-03 00:20:46 +0300

    kelime listesi eklendi ve rasgele kelime seçim fonksiyonu oluşturuldu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./App.css";
+import words from "./wordList.json";
 
 function App() {
+  const [wordToGuess, SetWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string>([]);
+
+  console.log(wordToGuess);
+
   return <div className="w-full h-screen bg-neutral-800 text-white">Hello</div>;
 }
 
