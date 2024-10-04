04e22d3827687415e8b8402663dd60a332fce45f
erain <enesyagmur99@gmail.com>
2024-10-05 01:01:32 +0300

    tıklanan tuşları yakalama ve guess componentine gönderme işlemi yapıldı

--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -1,25 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
+
 type GuessProps = {
   sipletedWord: string[];
+  guess: string[];
 };
 
-const Guess: React.FC<GuessProps> = ({ sipletedWord }) => {
-  console.log(sipletedWord);
+const Guess: React.FC<GuessProps> = ({ sipletedWord, guess }) => {
+  console.log("gues componentinde: ", guess);
 
   return (
     <div className="w-full h-[100px] flex flex-col items-center justify-between mt-8">
       <div className="w-full h-[80px] flex items-center justify-center">
-        {sipletedWord.map((char, index) => (
-          <input
-            type="text"
-            maxLength={1}
-            className="guess-input"
-            key={index + char}
-          />
+        {sipletedWord.map(() => (
+          <p className="guess-char" key={uuidv4()}></p>
         ))}
       </div>
       <div className="w-full h-[20px] flex items-center justify-center">
-        {sipletedWord.map((index) => (
-          <span className="guess-line" key={index}></span>
+        {sipletedWord.map(() => (
+          <span className="guess-line" key={uuidv4()}></span>
         ))}
       </div>
     </div>
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,4 +1,12 @@
-const Keyboard = () => {
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+type KeyboardProps = {
+  guess: string[];
+  setGuess: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+const Keyboard: React.FC<KeyboardProps> = ({ guess, setGuess }) => {
   const letters: string[] = [
     "A",
     "B",
@@ -27,11 +35,23 @@ const Keyboard = () => {
     "Y",
     "Z",
   ];
+  const [guessCounter, setGuessCounter] = useState<number>(0);
 
-  return (
-    <div className="w-8/12  flex flex-wrap justify-center mt-10 ">
-      {letters.map((letter, index) => (
-        <button className="keyboard-button" key={index}>
+  const [visibleKeyboard, setVisibleKeyboard] = useState<boolean>(false);
+
+  const charGuessFunc = (char: string) => {
+    setGuess((prevGuess) => [...prevGuess.slice(0, guessCounter), char]);
+    setGuessCounter(guessCounter + 1);
+  };
+
+  return visibleKeyboard ? null : (
+    <div className="w-8/12 flex flex-wrap justify-center mt-10 ">
+      {letters.map((letter) => (
+        <button
+          className="keyboard-button"
+          key={uuidv4()}
+          onClick={() => charGuessFunc(letter)}
+        >
           {letter}
         </button>
       ))}
