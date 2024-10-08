bf2691e63d6e1443c32cd034eea31b790414a073
erain <enesyagmur99@gmail.com>
2024-10-08 03:03:53 +0300

    hata sayısına göre head ikonunu değiştirme ve oyun bittiğinde tahmini gizleme

--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
+import { stateRoot } from "../Redux/store";
+import { useSelector } from "react-redux";
 
 type GuessProps = {
   splitedWord: string[];
@@ -6,22 +8,26 @@ type GuessProps = {
 };
 
 const Guess: React.FC<GuessProps> = ({ splitedWord, guess }) => {
-  return (
-    <div className="w-[350px] md:w-[785px] lg:w-[935px] xl:w-[1190px] h-[50px] md:h-[100px] flex flex-col items-center  mt-8 bg-white rounded-xl">
-      <div className="w-full h-[35px] md:h-[50px] flex items-center justify-center md:mt-4">
-        {guess.map((char) => (
-          <p className="guess-char" key={uuidv4()}>
-            {char}
-          </p>
-        ))}
+  const errorCount = useSelector((state: stateRoot) => state.errorStore.count);
+
+  if (errorCount !== 5) {
+    return (
+      <div className="w-[350px] md:w-[785px] lg:w-[935px] xl:w-[1190px] h-[50px] md:h-[100px] flex flex-col items-center  mt-8 bg-white rounded-xl">
+        <div className="w-full h-[35px] md:h-[50px] flex items-center justify-center md:mt-4">
+          {guess.map((char) => (
+            <p className="guess-char" key={uuidv4()}>
+              {char}
+            </p>
+          ))}
+        </div>
+        <div className="w-full h-[15px] md:h-[30px] flex items-center justify-center mb-1 md:mb-3">
+          {splitedWord.map(() => (
+            <span className="guess-line" key={uuidv4()}></span>
+          ))}
+        </div>
       </div>
-      <div className="w-full h-[15px] md:h-[30px] flex items-center justify-center mb-1 md:mb-3">
-        {splitedWord.map(() => (
-          <span className="guess-line" key={uuidv4()}></span>
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Guess;
--- src/components/hangman/Head.tsx
+++ src/components/hangman/Head.tsx
@@ -1,11 +1,45 @@
-import { FaRegFaceMeh } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+import { stateRoot } from "../../Redux/store";
+import { FaFaceSmile } from "react-icons/fa6";
+import { FaFaceMeh } from "react-icons/fa6";
+import { FaFaceFrown } from "react-icons/fa6";
+import { FaFaceFrownOpen } from "react-icons/fa6";
+import { FaFaceDizzy } from "react-icons/fa6";
 
 const Head = () => {
-  return (
-    <div className="w-full h-[50px] flex justify-center items-center absolute top-[40px] m-auto ">
-      <FaRegFaceMeh className="w-[50px] h-[50px] " />
-    </div>
-  );
+  const errorCount = useSelector((state: stateRoot) => state.errorStore.count);
+
+  if (errorCount === 1) {
+    return (
+      <div className="w-full h-[50px] flex justify-center items-center absolute top-[40px] m-auto ">
+        <FaFaceSmile className="w-[50px] h-[50px] " />
+      </div>
+    );
+  } else if (errorCount === 2) {
+    return (
+      <div className="w-full h-[50px] flex justify-center items-center absolute top-[40px] m-auto ">
+        <FaFaceMeh className="w-[50px] h-[50px] " />
+      </div>
+    );
+  } else if (errorCount === 3) {
+    return (
+      <div className="w-full h-[50px] flex justify-center items-center absolute top-[40px] m-auto ">
+        <FaFaceFrown className="w-[50px] h-[50px] " />
+      </div>
+    );
+  } else if (errorCount === 4) {
+    return (
+      <div className="w-full h-[50px] flex justify-center items-center absolute top-[40px] m-auto ">
+        <FaFaceFrownOpen className="w-[50px] h-[50px] " />
+      </div>
+    );
+  } else if (errorCount === 5) {
+    return (
+      <div className="w-full h-[50px] flex justify-center items-center absolute top-[40px] m-auto ">
+        <FaFaceDizzy className="w-[50px] h-[50px] " />
+      </div>
+    );
+  }
 };
 
 export default Head;
